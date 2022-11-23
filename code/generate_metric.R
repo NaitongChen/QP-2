86cf0188c7a2de35d58620264088a777798a55f4
chennaitong218@hotmail.com <chennaitong218@hotmail.com>
2022-11-22 18:42:17 -0800

    finished code for reproducing table 2

---
+++ code/generate_metric.R
@@ -0,0 +1,79 @@
+hyperparams = read.csv('hyperparams.csv');
+Lambda.Alpha = hyperparams[,1:2]
+Lambda = hyperparams[,3]
+
+K=100;
+CASE=3;
+l2loss = matrix(0,K,CASE);
+l1loss = matrix(0,K,CASE);
+FP = matrix(0,K,CASE);
+FN = matrix(0,K,CASE);
+RA_1 = matrix(0,K,CASE);
+RA_2 = matrix(0,K,CASE);
+
+l2loss_lasso = matrix(0,K,CASE);
+l1loss_lasso = matrix(0,K,CASE);
+FP_lasso = matrix(0,K,CASE);
+FN_lasso = matrix(0,K,CASE);
+
+X = read.csv('x_ho.csv');
+Y = read.csv('y_ho.csv');
+
+for (k in 1:K) {
+  Betar = matrix(0,p,CASE);
+  Beta_lasso = matrix(0,p,CASE);
+  for (j in 1:CASE) {
+    print(c(k,j))
+    Betar[,j] = L1PenHuber(Y[(n*(k-1)+1):(n*k),j], X[(n*(k-1)+1):(n*k),], Lambda.Alpha[j,1], Lambda.Alpha[j,2]);
+    Betar[,j] = Betar[,j] * (abs(Betar[,j]) > 1e-04);
+    
+    a = glmnet(x = X[(n*(k-1)+1):(n*k),], y = Y[(n*(k-1)+1):(n*k), j],
+               family = "gaussian", alpha = 1, lambda = c(Lambda[j], 0), intercept = FALSE);
+    Beta_lasso[,j] = a$beta[,1]
+    
+    l2loss[k,j] = norm(Betar[,j] - beta_0, "2");
+    l1loss[k,j] = norm(Betar[,j] - beta_0, "1");
+    
+    l2loss_lasso[k,j] = norm(Beta_lasso[,j] - beta_0, "2");
+    l1loss_lasso[k,j] = norm(Beta_lasso[,j] - beta_0, "1");
+    
+    temp = Betar[,j];
+    FP[k,j] = sum(temp[which(beta_0 == 0)] != 0);
+    FN[k,j] = sum(temp[which(beta_0 != 0)] == 0);
+    
+    temp = Beta_lasso[,j];
+    FP_lasso[k,j] = sum(temp[which(beta_0 == 0)] != 0);
+    FN_lasso[k,j] = sum(temp[which(beta_0 != 0)] == 0);
+    
+    
+    df = as.data.frame(cbind(Y[(n*(k-1)+1):(n*k),j], X[(n*(k-1)+1):(n*k),1:20]))
+    colnames(df)[1] = "Y"
+    fit = lm(formula = Y ~ . - 1, data = df)
+    beta_oracle = fit$coefficients
+    RA_1[k,j] = (norm(Beta_lasso[,j] - beta_0, "2") - norm(beta_oracle - beta_0, "2")) / (norm(Betar[,j] - beta_0, "2") - norm(beta_oracle - beta_0, "2"))
+    RA_2[k,j] = (norm(Beta_lasso[,j] - beta_0, "1") - norm(beta_oracle - beta_0, "1")) / (norm(Betar[,j] - beta_0, "1") - norm(beta_oracle - beta_0, "1"))
+  }
+}
+
+mean(l1loss)
+quantile(l1loss, probs = c(0.05, 0.95))
+mean(l2loss)
+quantile(l2loss, probs = c(0.05, 0.95))
+mean(FP)
+quantile(FP, probs = c(0.05, 0.95))
+mean(FN)
+quantile(FN, probs = c(0.05, 0.95))
+
+mean(l1loss_lasso)
+quantile(l1loss_lasso, probs = c(0.05, 0.95))
+mean(l2loss_lasso)
+quantile(l2loss_lasso, probs = c(0.05, 0.95))
+mean(FP_lasso)
+quantile(FP_lasso, probs = c(0.05, 0.95))
+mean(FN_lasso)
+quantile(FN_lasso, probs = c(0.05, 0.95))
+
+mean(RA_1)
+quantile(RA_1, probs = c(0.05, 0.95))
+mean(RA_2)
+quantile(RA_2, probs = c(0.05, 0.95))
--- code/main_simulation.R
+++ code/main_simulation.R
@@ -3,20 +3,6 @@ library(glmnet)
 library(doParallel)
 library(foreach)
 
-# set up parallel computing
-n.cores <- parallel::detectCores() - 1;
-
-my.cluster <- parallel::makeCluster(
-  n.cores,
-  type = "PSOCK"
-);
-
-doParallel::registerDoParallel(cl = my.cluster);
-
-foreach::getDoParRegistered()
-foreach::getDoParWorkers()
-
-
 n = 100;  # number of observations
 p = 400;  # number of variables
 CASE = 3;  # 3 scenarios for error distribution
@@ -32,7 +18,7 @@ beta_0 = as.matrix(c(3*rep(1,20), rep(0,380)))  # true beta
 XV = read.csv('x_ho_v.csv');
 YV = read.csv('y_ho_v.csv');
 lamb = seq(0.0001,LAMAX,length.out = LENGTH_la);
-lamb_lasso = seq(0.00001,LAMAX_lasso,length.out = LENGTH_la_lasso);
+lamb_lasso = rev(seq(0.00001,LAMAX_lasso,length.out = LENGTH_la_lasso));
 alp = seq(0.0001,AMAX,length.out = LENGTH_a);
 
 Lambda.Alpha = matrix(0, CASE, 2);  # stores optimal pair of (lambda, alpha) for 4 scenarios
@@ -54,27 +40,44 @@ L1PenHuber <- function(Y, X, lambda, alpha) {
   return(result$getValue(betaHat))
 }
 
+# set up parallel computing
+n.cores <- parallel::detectCores() - 1;
+
+my.cluster <- parallel::makeCluster(
+  n.cores,
+  type = "PSOCK"
+);
+
+doParallel::registerDoParallel(cl = my.cluster);
+
+foreach::getDoParRegistered()
+foreach::getDoParWorkers()
+
+clusterExport(cl=my.cluster, list("n", "p", "CASE", "LAMAX", "LAMAX_lasso", "AMAX", 
+                                  "LENGTH_la","LENGTH_la_lasso", "LENGTH_a", "N", "beta_0",
+                                  "XV", "YV", "lamb", "lamb_lasso", "alp"),
+              envir=environment())
+
 TuneHyperParam <- function(case) {
   loss1 = matrix(0, LENGTH_la, LENGTH_a);
   for (k in 1:N) {
-    for (i in 1:LENGTH_la) {
-      print(c(k,i))
-      loss1_i = foreach (j = 1:LENGTH_a, .combine = 'c', .packages = "CVXR") %dopar% {
+    print(k)
+    loss1_ij = foreach (i = 1:LENGTH_la, .combine = 'cbind', .packages = "CVXR", .export = "L1PenHuber") %:%
+      foreach (j = 1:LENGTH_a, .combine = 'c', .packages = "CVXR", .export ="L1PenHuber") %dopar% {
         betah = L1PenHuber(YV[(n*(k-1)+1):(n*k), case], XV[(n*(k-1)+1):(n*k),], lamb[i], alp[j]);
         betah = betah * (abs(betah) > 1e-04);
         betah = norm(betah-beta_0, "F");
       }
-      loss1[i,] = loss1[i,] + loss1_i;
-    }
+    loss1 = loss1 + t(loss1_ij)
   }
   
   loss1_lasso = matrix(0, LENGTH_la_lasso);
   for (k in 1:N) {
+    print(k)
     a <- glmnet(x = XV[(n*(k-1)+1):(n*k),], y = YV[(n*(k-1)+1):(n*k), case],
                 family = "gaussian", alpha = 1, lambda = lamb_lasso, intercept = FALSE)
     
     for (i in 1:LENGTH_la_lasso) {
-      print(c(k,i))
       betah = a$beta[,i]
       betah = betah * (abs(betah) > 1e-04);
       loss1_lasso[i] = loss1_lasso[i] + norm(betah-beta_0, "F");
@@ -87,29 +90,34 @@ TuneHyperParam <- function(case) {
   
   ind = which(loss1_lasso == min(loss1_lasso))
   
-  return(c(q1, q2, ind))
+  return(list(q1, q2, ind, loss1, loss1_lasso))
 }
 
 # 1st scenario: N(0,4)
 inds = TuneHyperParam(1);
-q1 = inds[1];
-q2 = inds[2];
+q1 = inds[[1]];
+q2 = inds[[2]];
 Lambda.Alpha[1,1] = lamb[q1];
 Lambda.Alpha[1,2] = alp[q2];
-Lambda[1] = lamb_lasso[inds[3]]
+Lambda[1] = lamb_lasso[inds[[3]]];
 
 # 2nd scenario: 2t_3
 inds = TuneHyperParam(2);
-q1 = inds[1];
-q2 = inds[2];
+q1 = inds[[1]];
+q2 = inds[[2]];
 Lambda.Alpha[2,1] = lamb[q1];
 Lambda.Alpha[2,2] = alp[q2];
-Lambda[2] = lamb_lasso[inds[3]]
+Lambda[2] = lamb_lasso[inds[[3]]];
 
 # 3rd scenario: MixN
 inds = TuneHyperParam(3);
-q1 = inds[1];
-q2 = inds[2];
+q1 = inds[[1]];
+q2 = inds[[2]];
 Lambda.Alpha[3,1] = lamb[q1];
 Lambda.Alpha[3,2] = alp[q2];
-Lambda[3] = lamb_lasso[inds[3]]
+Lambda[3] = lamb_lasso[inds[[3]]];
+
+parallel::stopCluster(my.cluster)
+
+output = as.data.frame(cbind(Lambda.Alpha, Lambda))
+write.csv(output, file = "hyperparams.csv", row.names = FALSE)
